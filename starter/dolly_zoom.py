d1a7a9d72beb127bb5cc58f9faa8d26ba0eb4491
zimingg <zimingg2001@gmail.com>
2025-09-03 15:50:37 -0400

    Fixed dolly zoom

--- starter/dolly_zoom.py
+++ starter/dolly_zoom.py
@@ -33,9 +33,13 @@ def dolly_zoom(
     fovs = torch.linspace(5, 120, num_frames)
 
     renders = []
+    
+    width = 5 # from Wikipedia formula
     for fov in tqdm(fovs):
-        distance = 3  # TODO: change this.
-        T = [[0, 0, 3]]  # TODO: Change this.
+        # distance = 50
+        distance = width / (2 * np.tan(0.5 * np.radians(fov))) # TODO: change this.
+        print(distance)
+        T = [[0, 0, distance]]  # TODO: Change this.
         cameras = pytorch3d.renderer.FoVPerspectiveCameras(fov=fov, T=T, device=device)
         rend = renderer(mesh, cameras=cameras, lights=lights)
         rend = rend[0, ..., :3].cpu().numpy()  # (N, H, W, 3)
@@ -47,14 +51,14 @@ def dolly_zoom(
         draw = ImageDraw.Draw(image)
         draw.text((20, 20), f"fov: {fovs[i]:.2f}", fill=(255, 0, 0))
         images.append(np.array(image))
-    imageio.mimsave(output_file, images, duration=duration)
+    imageio.mimsave(output_file, images, duration=duration, loop=0)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--num_frames", type=int, default=10)
     parser.add_argument("--duration", type=float, default=3)
-    parser.add_argument("--output_file", type=str, default="images/dolly.gif")
+    parser.add_argument("--output_file", type=str, default="images/dolly2.gif")
     parser.add_argument("--image_size", type=int, default=256)
     args = parser.parse_args()
     dolly_zoom(
